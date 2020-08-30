41677b75c8fdabe0546bc94378797c6e08527dd3
Alex <kavanokami@gmail.com>
2020-08-30 19:22:25 +0400

    feat: handle sign in with google

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { signInWithGoogle } from '../helpers/auth';
 
 const LogIn = () => {
-  return <div>Please log in with Google, Log in</div>;
+  const [error, setError] = useState(false);
+
+  const handleSignIn = async () => {
+    try {
+      await signInWithGoogle();
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      Please log in with Google, Log in
+      <button onClick={handleSignIn}>Sign in with Google</button>
+      {error && error}
+    </div>
+  );
 };
 
 export default LogIn;
