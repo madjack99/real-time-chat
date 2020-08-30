c49a2280f47ed182a8bb1f27c69914fe89e52cd6
Alex <kavanokami@gmail.com>
2020-08-30 18:50:02 +0400

    feat: add onAuthStateChange logic to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
@@ -11,12 +11,29 @@ import PublicRoute from './hoc/PublicRoute';
 import { auth } from './services/firebase';
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    auth().onAuthStateChanged((user) => {
+      if (user) setAuthenticated(true);
+      else setAuthenticated(false);
+    });
+  });
+
   return (
     <Router>
       <Switch>
         <Route exact path='/' component={Home} />
-        <PublicRoute path='/login' component={LogIn} authenticated={false} />
-        <PrivateRoute path='/chat' component={Chat} authenticated={false} />
+        <PublicRoute
+          path='/login'
+          component={LogIn}
+          authenticated={authenticated}
+        />
+        <PrivateRoute
+          path='/chat'
+          component={Chat}
+          authenticated={authenticated}
+        />
       </Switch>
     </Router>
   );
